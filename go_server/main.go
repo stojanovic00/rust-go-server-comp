650d1934f35adba1659d328e0cb8067b87d38ad6
Aleksandar <al.stojanovic0@gmail.com>
2023-12-19 15:57:27 +0100

    (Go) Implemented thread pool

--- go_server/main.go
+++ go_server/main.go
@@ -3,14 +3,66 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 )
 
+type ShutdowServerFlag struct {
+	sync.Mutex
+	close bool
+}
+
+func NewShutdowServerFlag() *ShutdowServerFlag {
+	return &ShutdowServerFlag{
+		Mutex: sync.Mutex{},
+		close: false,
+	}
+}
+
 func main() {
+	//Allocating resources
+	repo := NewRepo()
+	mapMux := &sync.RWMutex{}
+
+	//Preparing thread pool
+	poolSizeStr := os.Getenv("THREAD_POOL_SIZE")
+	poolSize, err := strconv.Atoi(poolSizeStr)
+	if err != nil {
+		log.Fatal("Pool size not defined")
+	}
+	fmt.Printf("Thread pool size: %d\n", poolSize)
+
+	connChan := make(chan net.Conn)
+	wg := sync.WaitGroup{}
+
+	//Starting thread pool
+	for i := 0; i < poolSize; i++ {
+		wg.Add(1)
+		go func(threadNum int) {
+			defer wg.Done()
+
+			for {
+				select {
+				case conn, ok := <-connChan:
+					{
+						if !ok {
+							return
+						}
+						fmt.Printf("Thread %d handles request\n", threadNum)
+						handleConnection(conn, repo, mapMux)
+					}
+				}
+			}
+		}(i)
+	}
+
+	//Listening for requests
 	tcp_address := os.Getenv("TCP_ADDRESS")
 	listener, err := net.Listen("tcp", tcp_address)
 	if err != nil {
@@ -20,17 +72,57 @@ func main() {
 	defer listener.Close()
 	fmt.Printf("Listening for tcp requests on: %s\n", tcp_address)
 
-	repo := NewRepo()
-	mapMux := &sync.RWMutex{}
+	shutdownServerFlag := NewShutdowServerFlag()
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting:", err)
+				continue
+			}
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting:", err)
-			continue
+			//Check for termination
+			shutdownServerFlag.Lock()
+			if shutdownServerFlag.close {
+				close(connChan)
+				shutdownServerFlag.Unlock()
+				return
+			} else {
+				shutdownServerFlag.Unlock()
+			}
+
+			//Dispatch request to thread pool
+			connChan <- conn
 		}
-		go handleConnection(conn, repo, mapMux)
+	}()
+
+	//Graceful shutdown
+	interruptCh := make(chan os.Signal, 1)
+	signal.Notify(interruptCh, os.Interrupt, syscall.SIGTERM)
+
+	<-interruptCh
+	shutdownServerFlag.Lock()
+	shutdownServerFlag.close = true
+	shutdownServerFlag.Unlock()
+
+	sendShutdowRequest(tcp_address)
+
+	fmt.Printf("Waiting for threads to close...\n")
+	wg.Wait()
+	fmt.Printf("Server shutdown\n")
+}
+
+// This is used to unstuck for loop  that waits for connection so it can terminate
+func sendShutdowRequest(tcp_address string) {
+	shutdownConn, err := net.Dial("tcp", tcp_address)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		//TODO error handling
 	}
+	defer shutdownConn.Close()
+	// Send an HTTP GET request
+	request := "GET / HTTP/1.1\r\n\r\n"
+	_, err = fmt.Fprintf(shutdownConn, request)
 }
 
 func handleConnection(conn net.Conn, repo *Repo, mapMux *sync.RWMutex) {
@@ -38,8 +130,9 @@ func handleConnection(conn net.Conn, repo *Repo, mapMux *sync.RWMutex) {
 
 	request, err := ParseRequest(conn)
 	if err != nil {
-		response := "HTTP/1.1 400 BadRequest\r\nConnection: close\r\n\r\n"
+		response := "HTTP/1.1 400 BadRequest\r\nConnection: close\r\n\r\n" + err.Error()
 		conn.Write([]byte(response))
+		return
 	}
 
 	switch request.Method {
